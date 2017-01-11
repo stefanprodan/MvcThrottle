b8c2aa5abdd3142f275da1dd8232f0991f59b263
Stefan Prodan <stefan.prodan@gmail.com>
2017-01-12 01:14:22 +0200

    v2.0.0 release

--- MvcThrottle/Properties/AssemblyInfo.cs
+++ MvcThrottle/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MvcThrottle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("stefanprodan.eu")]
+[assembly: AssemblyCompany("stefanprodan.com")]
 [assembly: AssemblyProduct("MvcThrottle")]
-[assembly: AssemblyCopyright("Copyright © Stefan Prodan 2014")]
+[assembly: AssemblyCopyright("Copyright © Stefan Prodan 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
